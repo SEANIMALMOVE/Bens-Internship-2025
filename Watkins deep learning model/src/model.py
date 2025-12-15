5bf5300fe3cd72c5bc6860d9865fe00d807171fa
beno7o <beno7beno7@gmail.com>
2025-12-15 12:17:28 +0100

    model training sketch
    + project orgenising

--- Watkins deep learning model/src/model.py
+++ Watkins deep learning model/src/model.py
@@ -30,3 +30,42 @@ class EfficientNetSpectrogram(nn.Module):
 
         # Forward through EfficientNet
         return self.backbone(x)
+
+
+
+# Define a simple CNN model for spectrogram classification
+class BaselineCNN(nn.Module):
+    def __init__(self, input_channels=1, num_classes=2):
+        super().__init__()
+
+        # extracts features from spectrogram
+        self.features = nn.Sequential(
+            # low level patterns (edges, textures)
+            nn.Conv2d(input_channels, 16, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2), 
+            # more complex patterns (harmonics, frequency bands)
+            nn.Conv2d(16, 32, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
+            # high level structures (shapes of vocyyalizations)
+            nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2), 
+        )   
+            ### -> 1-16-32-64 the model learns more complex feature maps
+            # MaxPool2d = width decreases by factor of 2 each time
+            # height also gets reduced 
+            ### -> [batch_size, 64, 16, reduced_width]
+
+
+        # converts extracted features to class decisions   
+        self.classifier = nn.Sequential(
+            nn.Flatten(), # -> turns feature map into vector
+
+            # nn.Linear(64 *  (64) * (64), 128),  # size of input spectrogram 
+            nn.LazyLinear(128), # automatically
+
+            nn.ReLU(), # non-linear activation
+            nn.Dropout(0.4), # randomly turn off 40% of neurons during training to prevent overfitting
+            nn.Linear(128, num_classes) # output layer
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
---
+++ Watkins deep learning model/src/train.py
@@ -0,0 +1,9 @@
+## 1. forward pass: spectogram -> model -> logits
+## 2. loss function: given the logits and the true label how bad is the prediction?
+## 3. backward pass: compute gradients: which weights caused this mistake, and in what direction should they change
+## 4. optimizer step: update weights based on gradients and learning rate just a tiny bit
+
+# before training: outputs are noisy, logits are small and similar, predictions are random
+# after training: output correct class logits become larger, wrong class logits become smaller, loss decreases, accuracy increases (shape of tensors dont change, only the meaning of the numbers)
+
+
